e57381d2587efb5250cbfe79c51e873171af9642
Ochiengsteven <stevenochieng6305@gmail.com>
2023-08-15 14:14:06 +0300

    ADD HOME,CALCULATOR AND QUOTE SECTIONS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
 import Calculator from './components/calculator';
 import QuoteDisplay from './components/QuoteDisplay';
 
 export default function App() {
   return (
-    <div className="app">
-      <Calculator />
-      <QuoteDisplay />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/calculator" element={<Calculator />} />
+      <Route path="/quote" element={<QuoteDisplay />} />
+    </Routes>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <h3>Welcome to Math Magicians</h3>
+    <p>
+      Explore the fascinating world of mathematics! Whether you are a student, an educator,
+      or simply curious about math, Math Magicians has something for everyone. From
+      basic calculations to advanced concepts, we have got you covered.
+    </p>
+  </div>
+);
+
+export default Home;
