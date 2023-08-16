1d2ccdf22817d12a7b4c296ab36a64846f2d53b9
riskmaris <mariskaps20@gmail.com>
2023-08-16 15:56:21 +0300

    add tests

--- src/logic/tests/operate.test.js
+++ src/logic/tests/operate.test.js
@@ -20,4 +20,16 @@ describe('operate function', () => {
   it('handles division by zero', () => {
     expect(operate(6, 0, '/')).toBe("Can't divide by 0.");
   });
+
+  it('calculates modulo', () => {
+    expect(operate(7, 3, '%')).toBe('1');
+  });
+
+  it('handles modulo by zero', () => {
+    expect(operate(7, 0, '%')).toBe("Can't find modulo as can't divide by 0.");
+  });
+
+  it('throws error for unknown operation', () => {
+    expect(() => operate(2, 3, '^')).toThrow("Unknown operation '^'");
+  });
 });
